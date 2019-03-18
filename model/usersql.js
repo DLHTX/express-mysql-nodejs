4d694b27d7f31e991944349a911f9bad74baa307
DLHTX <35157091+DLHTX@users.noreply.github.com>
2019-03-18 15:15:38 +0800

    Update usersql.js

--- model/usersql.js
+++ model/usersql.js
@@ -1,5 +1,5 @@
 var UserSQL = {  
-    insert:'INSERT INTO test(uid,name) VALUES(uid,name)', 
+    insert:'INSERT INTO test(uid,name) VALUES(?,?)', 
     queryAll:'SELECT * FROM test',  
     getUserById:'SELECT * FROM test WHERE uid = ? ',
     test:'SELECT * FROM test',
